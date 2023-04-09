e0bd91d9a5d9fe225999bf84f5d4b8205b28368a
Anthony M <anthony.moffa@hey.com>
2023-04-09 12:23:05 -0400

    Add JournalEntry#display_title

--- app/models/journal_entry.rb
+++ app/models/journal_entry.rb
@@ -1,3 +1,19 @@
 class JournalEntry < ApplicationRecord
   has_rich_text :content
+
+  def display_title
+    if persisted?
+      title.present? ? title_with_date : created_at_string
+    else
+      Date.today.strftime('%B %-d, %Y')
+    end
+  end
+
+  def title_with_date
+    "#{created_at_string}: #{title}"
+  end
+
+  def created_at_string
+    created_at.strftime('%B %-d, %Y')
+  end
 end
