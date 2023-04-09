74aef879c9e9ab59ede2f886c9a4937d530a92b1
Anthony M <anthony.moffa@hey.com>
2023-04-09 11:31:00 -0400

    Add some basic layout and styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   resources :journal_entries
-  root 'journal_entries#index'
+  root 'journal_entries#new'
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,23 +1,32 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
   content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    "./public/*.html",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/views/**/*.{erb,haml,html,slim}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        "slide-down-up": {
+          "0%, 100%": { transform: "translateY(-3.5rem)" },
+          "30%, 70%": { transform: "translateY(0rem)" },
+        },
+      },
+      animation: {
+        "slide-down-up": "slide-down-up 3s ease-in-out forwards",
       },
     },
   },
   plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-    require('@tailwindcss/container-queries'),
-  ]
-}
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
