93f029fb67b8895768cef93251f8cab1e3ce476b
Anthony M <anthony.moffa@hey.com>
2023-04-16 10:53:21 -0400

    Add basic auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with(
+    name: Rails.application.credentials.http_basic_auth.name,
+    password: Rails.application.credentials.http_basic_auth.password
+  )
 end
