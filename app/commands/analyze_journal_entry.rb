0921f3de7a054188c13eb9832e4b97eef3166be7
Anthony M <anthony.moffa@hey.com>
2023-04-23 22:16:12 -0400

    Improve journal entry analysis slightly
    This commit changes two things about the journal entry analysis:
    
    1. It clears up any existing relationship summary embeddings related to
       the current journal entry for each person
    1. It wraps everything in a transaction just in case something goes
       wrong deleting or updating data
    
    These changes make it nicer when you edit a journal entry and prevents
    any data from getting lost if something goes wrong.

--- app/commands/analyze_journal_entry.rb
+++ app/commands/analyze_journal_entry.rb
@@ -8,26 +8,41 @@ class AnalyzeJournalEntry
   end
 
   def run
-    analysis = FetchJournalEntryAnalysis.run(journal_entry)
-    relationship_analysis = FetchJournalEntryRelationshipAnalysis.run(journal_entry)
-
-    people = relationship_analysis.map do |analysis|
-      Person.find_or_create_by(name: analysis[:name]).tap do |person|
-        embeddings = FetchEmbeddings.run(analysis[:summary])
-        embeddings.each do |embedding|
-          person.embeddings.create(
-            content: embedding[:chunk_text],
-            embedding: embedding[:embedding],
-            journal_entry:
-          )
-        end
+    ActiveRecord::Base.transaction do
+      # Fetch an analysis of the journal entry content
+      analysis = FetchJournalEntryAnalysis.run(journal_entry)
+      # Fetch a summary of the relationships in the journal entry content
+      relationship_analysis = FetchJournalEntryRelationshipAnalysis.run(journal_entry)
+      # Process the analysis and relationship analysis into people and embeddings
+      people = relationship_analysis.map do |person_data|
+        process_relationship_analysis(person_data)
       end
+      # Update the journal entry with the analysis and people
+      journal_entry.update(analysis:, people:)
     end
-
-    journal_entry.update(analysis:, people:)
   end
 
   private
 
   attr_reader :journal_entry
+
+  def create_embedding_for_entry(person, embedding_data)
+    person.embeddings.create!(
+      content: embedding_data[:chunk_text],
+      embedding: embedding_data[:embedding],
+      journal_entry:
+    )
+  end
+
+  def clear_existing_embeddings_for_entry(person)
+    person.embeddings.where(journal_entry:).destroy_all
+  end
+
+  def process_relationship_analysis(person_data)
+    Person.find_or_create_by(name: person_data[:name]).tap do |person|
+      generated_embeddings = FetchEmbeddings.run(person_data[:summary])
+      clear_existing_embeddings_for_entry(person)
+      generated_embeddings.each { |embedding| create_embedding_for_entry(person, embedding) }
+    end
+  end
 end
