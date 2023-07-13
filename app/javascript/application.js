f7c0b8e240182b69c26b950fb15313ef36e2057b
Anthony M <anthony.moffa@hey.com>
2023-07-13 09:44:17 -0400

    Try out a mobile layout for smaller screens
    There are two bits of javascript that are hacks to get the appearance
    just right in mobile browsers where the address bar might change the
    document flow in undesired ways.
    
    The `resizeMain` function makes it possible for the content to take up
    exactly the viewable area. This is a workaround that allows the footer
    to be visible without having the scroll the address bar away on android
    and other mobile browsers.
    
    The `adjustScrollableHeight` function forces the height of the main
    scrollable area on `div#scrollable` elements to fit to the screen. On
    mobile browsers, the address bar will sometimes push the bottom of the
    content off the viewable area, preventing the scroll from revealing all
    the content.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -5,6 +5,40 @@ import "@rails/actiontext";
 import "controllers";
 import feather from "feather-icons";
 
+// For smaller screens, set the height of the main element to the height of
+// the window so the footer can rest at the bottom of the page, regardless of
+// whether the address bar is visible or not. For larger screens, remove the
+// height so the content of the page can determine its own height.
+const resizeMain = () => {
+  const mainElement = document.querySelector("main");
+
+  if (window.innerWidth >= 640) {
+    mainElement.style.height = "";
+  } else {
+    const windowHeight = window.innerHeight;
+    mainElement.style.height = `${windowHeight}px`;
+  }
+};
+
+// Set the height of #scrollable elements to the height of the window so that it
+// can scroll its entire height. This is necessary because the bottom of
+// scrollable elements gets pushed off the bttom of the screen by the address
+// bar on mobile devices, obscuring the bottom content.
+const adjustScrollableHeight = () => {
+  const scrollable = document.getElementById("scrollable");
+  if (!scrollable) return;
+
+  const windowHeight = window.innerHeight;
+  scrollable.style.height = windowHeight + "px";
+};
+
 document.addEventListener("turbo:load", () => {
   feather.replace();
+  resizeMain();
+  adjustScrollableHeight();
+
+  window.addEventListener("resize", () => {
+    resizeMain();
+    adjustScrollableHeight();
+  });
 });
