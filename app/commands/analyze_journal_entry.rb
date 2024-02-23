f94b84b371dd6373a68cae98d7ac2dc1b1df6e84
Anthony M <anthony.moffa@hey.com>
2024-02-23 11:03:27 -0500

    Update how journal entry analysis works
    Due to some changes in rails and the application data model, a few
    tweaks were made to the AnalyzeJournalEntry command.
    
    1. The relationship summaries are built instead of created. Because the
       command might have just initialized a relationship record, create
       cannot be called until the parent relationship record has bene saved.
       Save is called all at once at the end of the relationship creation
       portion of the command
    2. Relationship records are now created in the context of the given
       journal entry's user. A user is required to create a relationship and
       this was previously failing.

--- app/commands/analyze_journal_entry.rb
+++ app/commands/analyze_journal_entry.rb
@@ -1,6 +1,7 @@
 class AnalyzeJournalEntry
   def initialize(journal_entry)
     @journal_entry = journal_entry
+    @user = journal_entry.user
   end
 
   def self.run(journal_entry)
@@ -25,10 +26,10 @@ class AnalyzeJournalEntry
 
   private
 
-  attr_reader :journal_entry
+  attr_reader :journal_entry, :user
 
-  def create_relationship_summary_for_entry(relationship, embedding_data)
-    relationship.relationship_summaries.create!(
+  def build_relationship_summary_for_entry(relationship, embedding_data)
+    relationship.relationship_summaries.build(
       content: embedding_data[:chunk_text],
       created_at: journal_entry.created_at,
       embedding: embedding_data[:embedding],
@@ -41,12 +42,19 @@ class AnalyzeJournalEntry
   end
 
   def process_relationship_analysis(relationship_data)
-    Relationship.find_or_create_by(name: relationship_data[:name]).tap do |relationship|
-      generated_embeddings = FetchEmbeddings.run(relationship_data[:summary])
-      clear_existing_relationship_summaries(relationship)
-      generated_embeddings.each do |embedding|
-        create_relationship_summary_for_entry(relationship, embedding)
+    user
+      .relationships
+      .find_or_create_by(name: relationship_data[:name])
+      .tap do |relationship|
+        generated_embeddings = FetchEmbeddings.run(relationship_data[:summary])
+
+        clear_existing_relationship_summaries(relationship)
+        generated_embeddings.each do |embedding|
+          build_relationship_summary_for_entry(relationship, embedding)
+        end
+
+        relationship.save!
+        relationship.relationship_summaries.each(&:save!)
       end
-    end
   end
 end
